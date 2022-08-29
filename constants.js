2e89715bc2fa8f8cb52e194da906d1bc855c06d4
narendra-sajwani <niks.imagination@gmail.com>
2022-08-29 12:22:16 +0530

    initial commit

---
+++ constants.js
@@ -0,0 +1,125 @@
+export const contractAddress = "0xf5D075b2c7bA8E8fDDb6Ffd6e50Dc6c98c3Bb57F"
+export const abi = [
+    {
+        inputs: [
+            {
+                internalType: "address",
+                name: "priceFeedAddress",
+                type: "address",
+            },
+        ],
+        stateMutability: "nonpayable",
+        type: "constructor",
+    },
+    {
+        inputs: [],
+        name: "FundMe__NotOwner",
+        type: "error",
+    },
+    {
+        stateMutability: "payable",
+        type: "fallback",
+    },
+    {
+        inputs: [],
+        name: "MINIMUM_USD",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "cheaperWithdraw",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "fund",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+    },
+    {
+        inputs: [
+            {
+                internalType: "address",
+                name: "funder",
+                type: "address",
+            },
+        ],
+        name: "getAddressToAmountFunded",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [
+            {
+                internalType: "uint256",
+                name: "index",
+                type: "uint256",
+            },
+        ],
+        name: "getFunder",
+        outputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "getOwner",
+        outputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "getPriceFeed",
+        outputs: [
+            {
+                internalType: "contract AggregatorV3Interface",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "withdraw",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+    },
+    {
+        stateMutability: "payable",
+        type: "receive",
+    },
+]
---
+++ index.js
@@ -0,0 +1,87 @@
+// In nodejs
+// require
+
+// In front-end javascript, you can't use require
+// import
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+        connectButton.innerHTML = "Connected!"
+    } else {
+        connectButton.innerHTML = "Please install MetaMask!"
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+    }
+}
+// fund function
+async function fund() {
+    // const ethAmount = "0.1"
+    const ethAmount = document.getElementById("fund").value
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        // provider/ connection to the blockchain
+        // signer/ wallet/ someone with some gas
+        // contract that we are interacting with
+        // ABI and Address
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            // hey, wait for this Tx to finish
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`)
+    // return new Promise()
+    // listen for this transaction to finish
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations`
+            )
+            resolve()
+        })
+    })
+}
+// withdraw function
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        console.log("Withdrawing...")
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
